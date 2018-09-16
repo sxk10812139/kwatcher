11225a7da1d3912290267ca91c6c67235b902443
Mu <sxk10812139@163.com>
2018-09-17 01:28:15 +0800

    init

---
+++ watcher.go
@@ -0,0 +1,80 @@
+package kwatcher
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+type Path string
+
+type Watcher struct {
+	WatchFiles []File
+	FileStatus map[Path]time.Time
+	Callback   func([]Path)
+	mux        *sync.Mutex
+}
+
+func (w *Watcher) AddFiles(f string) {
+	file := File{Path: Path(f)}
+	w.WatchFiles = append(w.WatchFiles, file)
+}
+
+func (w *Watcher) SetCallback(f func([]Path)) {
+	w.Callback = f
+}
+
+func (w *Watcher) init() {
+	for _, f := range w.WatchFiles {
+		fileInfo, err := os.Stat(string(f.Path))
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		w.FileStatus[f.Path] = fileInfo.ModTime()
+	}
+}
+
+func (w *Watcher) checkFiles() []Path {
+	var files []Path
+	for path, time := range w.FileStatus {
+		fileInfo, err := os.Stat(string(path))
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if fileInfo.ModTime() != time {
+			files = append(files, path)
+			w.FileStatus[path] = fileInfo.ModTime()
+		}
+	}
+	return files
+}
+
+func (w *Watcher) Run() {
+	w.init()
+	ticker := time.Tick(1 * time.Second)
+	for {
+		select {
+		case <-ticker:
+			go func() {
+				modifyFiles := w.checkFiles()
+				if len(modifyFiles) > 0 {
+					w.Callback(modifyFiles)
+				} else {
+					fmt.Println("周期执行")
+				}
+			}()
+		}
+	}
+
+}
+
+type File struct {
+	Path Path
+}
+
+func NewWatcher() *Watcher {
+	return &Watcher{FileStatus: make(map[Path]time.Time)}
+}
